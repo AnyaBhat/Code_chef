7e2a648631503170f013f5f939a7822a28bb7400
Anya Bhat <107057528+AnyaBhat@users.noreply.github.com>
2022-06-12 19:05:07 +0530

    Create EQUALDIST.cpp

---
+++ EQUALDIST.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,A,B;
+	cin>>T;
+	for(int i=0;i<T;i++)
+	{
+	    cin>>A>>B;
+	    if((A+B)%2==0)
+	        cout<<"YES"<<endl;
+	    else
+	        cout<<"NO"<<endl;
+	}
+	return 0;
+}
